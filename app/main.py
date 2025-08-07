026b60d8be1e8fcab7fc28c09f684dd3aee1f7b3
ayala-manuel <mayala@afi.es>
2025-08-07 11:10:58 +0200

    Docker and healthcheck

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routers.predict import router as predict_router
+from app.routers.health import router as health_router
 
 app = FastAPI()
 app.include_router(predict_router)
+app.include_router(health_router)
---
+++ app/routers/health.py
@@ -0,0 +1,9 @@
+# app/routers/health.py
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/health")
+async def health_check():
+    return {"status": "ok"}
